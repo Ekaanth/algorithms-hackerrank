22a2df3de62a7d92d0d766961c09c54b21e417bf
amalshehu <amalshehu@gmail.com>
2018-01-28 13:50:03 +0530

    Fix typos

--- Implementation/kangaroo/kangaroo.js
+++ Implementation/kangaroo/kangaroo.js
@@ -1,10 +1,12 @@
 function kangaroo(x1, v1, x2, v2) {
-    sameValueFound = false;
+    let sameValueFound = false;
+
     let kangOneJumpCountArray = [];
         kangOneJumpCountArray[0] = x1;
 
     let kangTwoJumpCountArray = [];
         kangTwoJumpCountArray[0]= x2;
+
     for (let i = 1; i < 10000; i++) {
         kangOneJumpCountArray[i] = kangOneJumpCountArray[i - 1] + v1;
         kangTwoJumpCountArray[i] =  kangTwoJumpCountArray[i - 1] + v2;
