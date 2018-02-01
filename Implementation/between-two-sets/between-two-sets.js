b77d697de30624e123149bcc2e6fe637359a41b7
amalshehu <amalshehu@gmail.com>
2018-02-01 15:57:21 +0530

    Find mods of all elements in Set A

--- Implementation/between-two-sets/between-two-sets.js
+++ Implementation/between-two-sets/between-two-sets.js
@@ -2,22 +2,46 @@ function getTotalX(a, b) {
     aFactors = [];
     bFactors = [];
     allFactorsArrayinA = new Set([])
+    allFactorsArrayinB = new Set([])
 
     a.map(number => {
-
         for (let i = 1; i <= number; i++) {
-        for (let j = 1; j <= 10; j++) {
-            // console.log(`${i} * ${j}=`, j * i );
-            const current = j * i;
-            if(current === number){
-                allFactorsArrayinA.add(i)
-            }
-
+					for (let j = 1; j <= 10; j++) {
+							const current = j * i;
+							if(current === number){
+									allFactorsArrayinA.add(i)
+							}
+					}
         }
+		})
+		b.map(number => {
+			for (let i = 1; i <= number; i++) {
+				for (let j = 1; j <= 10; j++) {
+						const current = j * i;
+						if(current === number){
+								allFactorsArrayinB.add(i)
+						}
+				}
+			}
+	})
+	let filteredSetA = new Set([]);
+	Array.from(allFactorsArrayinA).map(x => {
+	// console.log(d);
 
-        }
-    })
-    console.log(allFactorsArrayinA);
+	for (let i=0; i < a.length; i++){
+		let acc = [];
+		console.log(`${x} % ${a[i]}`,x % a[i]);
+
+		if (x % a[i] === 0 ){
+			filteredSetA.add(x);
+		}
+		
+	}
+	
+	})
+	
+	console.log(filteredSetA);
+		
     
 }
 getTotalX([ 2, 4 ], [ 16, 32, 96 ])
