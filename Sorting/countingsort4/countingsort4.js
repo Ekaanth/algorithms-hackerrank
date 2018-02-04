57c719d089f94c8eeba0ddddf6cc2ac412fc7232
amalshehu <amalshehu@gmail.com>
2018-02-04 14:12:36 +0530

    Fix run time errors

--- Sorting/countingsort4/countingsort4.js
+++ Sorting/countingsort4/countingsort4.js
@@ -1,39 +1,33 @@
-let arr = [
-  ['0', 'ab'],
-  ['6', 'cd'],
-  ['0', 'ef'],
-  ['6', 'gh'],
-  ['4', 'ij'],
-  ['0', 'ab'],
-  ['6', 'cd'],
-  ['0', 'ef'],
-  ['6', 'gh'],
-  ['0', 'ij'],
-  ['4', 'that'],
-  ['3', 'be'],
-  ['0', 'to'],
-  ['1', 'be'],
-  ['5', 'question'],
-  ['1', 'or'],
-  ['2', 'not'],
-  ['4', 'is'],
-  ['2', 'to'],
-  ['4', 'the']
-];
-for (let i = 0; i < (arr.length) / 2; i++) {
-  arr[i][1] = '-';
+function countingSort4(arr) {
+  for (let i = 0; i < (arr.length) / 2; i++) {
+ arr[i][1] = '-';
 }
 for (let i = 0; i < arr.length; i++) {
-  arr[i][2] = i;
+ arr[i][2] = i;
 }
 
 let compareNumbers = (a, b) => {
-  if (a[0] === b[0]) {
-    return a[2] - b[2];
-  }
-  return a[0] - b[0];
+ if (a[0] === b[0]) {
+   return a[2] - b[2];
+ }
+ return a[0] - b[0];
 }
 arr.sort(compareNumbers)
 
 let strings = arr.map(d => d[1])
-console.log(...strings);
+return [...strings];
+}
+
+
+function main() {
+   var arr = [];
+   var n = parseInt(readLine());
+   for(var a0 = 0; a0 < n; a0++){
+       var x_temp = readLine().split(' ');
+       var x = parseInt(x_temp[0]);
+       var s = x_temp[1];
+       arr.push(x_temp)
+   }
+   var result = countingSort4(arr);
+   console.log(result.join(" "));
+}
