83e97b61f153fb94e7096f9cc16078e1c36ea96f
amalshehu <amalshehu@gmail.com>
2018-02-06 14:19:34 +0530

    Add test cases

--- Implementation/angry-professor/angry-professor.js
+++ Implementation/angry-professor/angry-professor.js
@@ -1,15 +1,56 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
 function angryProfessor(k, a) {
   lateCount = 0;
   a.map(student => {
-    if(student >= 0){
+    if(student > 0){
       lateCount++;
     }
   })
-  if(lateCount < k){
+  // console.log(lateCount);
+  
+  if((a.length - lateCount) < k){
+    console.log('lateCount', lateCount, k);
+    
     return 'YES'
   }else{
+    console.log('lateCount', lateCount, k);
     return 'NO'
   }
   
 }
-console.log(angryProfessor(1, [88, -17, -96, 43, 83, 99, 25, 90, -39, 86]));
+// console.log(angryProfessor(7, [26, 94, -95, 34, 67, -97, 17, 52, 1, 86]));
+
+function main() {
+    var t = parseInt(readLine());
+    for(var a0 = 0; a0 < t; a0++){
+        var n_temp = readLine().split(' ');
+        var n = parseInt(n_temp[0]);
+        var k = parseInt(n_temp[1]);
+        a = readLine().split(' ');
+        a = a.map(Number);
+        var result = angryProfessor(k, a);
+        process.stdout.write("" + result + "\n");
+    }
+
+}
