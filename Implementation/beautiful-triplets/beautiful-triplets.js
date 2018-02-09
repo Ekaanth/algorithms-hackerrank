11307388c266a9b0e5e793dc8e354c6057a880ab
amalshehu <amalshehu@gmail.com>
2018-02-09 15:22:48 +0530

    Fix typos

--- Implementation/beautiful-triplets/beautiful-triplets.js
+++ Implementation/beautiful-triplets/beautiful-triplets.js
@@ -3,9 +3,11 @@ function beautifulTriplets(d, arr) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = i + 1; j < arr.length; j++) {
       for (let k = i + 2; k < arr.length; k++) {
-        if (arr[i] < arr[j] && arr[j] < arr[k]) {
-          if ((arr[j] - arr[i] === d && arr[k] - arr[j]) === d) {
-            count++;
+        if (arr[i] && arr[k] && arr[k]) {
+          if (arr[i] < arr[j] && arr[j] < arr[k]) {
+            if ((arr[j] - arr[i] === d && arr[k] - arr[j]) === d) {
+              count++;
+            }
           }
         }
       }
