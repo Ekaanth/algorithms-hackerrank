4d91150807de372fa60865a98b19c56412455c7e
amalshehu <amalshehu@gmail.com>
2018-02-09 14:40:06 +0530

    Add solution

--- Implementation/beautiful-triplets/beautiful-triplets.js
+++ Implementation/beautiful-triplets/beautiful-triplets.js
@@ -1,14 +1,16 @@
 function beautifulTriplets(d, arr) {
+  count = 0;
   for (let i = 0; i < arr.length; i++) {
     for (let j = i + 1; j < arr.length; j++) {
       for (let k = i + 2; k < arr.length; k++) {
         if (arr[i] < arr[j] && arr[j] < arr[k]) {
-          // if ((arr[j] - arr[i] === arr[k] - arr[j]) === d) {
-          console.log('i , k , j', [arr[i], arr[j], arr[k]]);
-          // }
+          if ((arr[j] - arr[i] === d && arr[k] - arr[j]) === d) {
+            count++;
+          }
         }
       }
     }
   }
+  return count;
 }
 beautifulTriplets(3, [1, 2, 4, 5, 7, 8, 10]);
