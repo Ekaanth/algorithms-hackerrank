fd49aa25ec12f3ec9bb5cfa27c1e33b4faca6c2a
amalshehu <amalshehu@gmail.com>
2018-02-10 14:12:35 +0530

    Add solution

--- Implementation/encryption/encryption.js
+++ Implementation/encryption/encryption.js
@@ -1,21 +1,18 @@
 function encryption(s) {
   s = s.replace(/\s+/g, '');
   str = s.split('');
-  console.log(s);
   len = s.length;
   rows = Math.floor(Math.sqrt(len));
   cols = Math.ceil(Math.sqrt(len));
   strMatrix = [];
-  if (rows * cols >= len) {
-    console.log('R', rows);
-    console.log('C', cols);
-    while (str.length) {
-      strMatrix.push(str.splice(0, cols));
-    }
+  while (str.length) {
+    strMatrix.push(str.splice(0, cols));
   }
-  console.log(strMatrix);
+
+  encrypted = '';
+  for (let i = 0; i < cols; i++) {
+    encrypted += strMatrix.map(c => c[i]).join('') + ' ';
+  }
+  return encrypted;
 }
-// encryption('haveaniceday');
-encryption(
-  'if man was meant to stay on the ground god would have given us roots'
-);
+encryption('chillout');
