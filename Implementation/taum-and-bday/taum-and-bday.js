77d0a974b55fd99ebb6349d270aa8135318a84b6
amalshehu <amalshehu@gmail.com>
2018-02-11 17:57:53 +0530

    Large number - auto round - issue - JS

--- Implementation/taum-and-bday/taum-and-bday.js
+++ Implementation/taum-and-bday/taum-and-bday.js
@@ -1,18 +1,58 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = '';
+var input_stdin_array = '';
+var input_currentline = 0;
+
+process.stdin.on('data', function(data) {
+  input_stdin += data;
+});
+
+process.stdin.on('end', function() {
+  input_stdin_array = input_stdin.split('\n');
+  main();
+});
+
+function readLine() {
+  return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
 function taumBday(b, w, x, y, z) {
   if ((x == y && y == z) || (x < z && y < z)) {
     return b * x + w * y;
   }
   if (x > y && x > z) {
+    let costs = [];
     let whiteToBlackCost = y + z;
-    return b * whiteToBlackCost + w * y;
+    c1 = b * whiteToBlackCost + w * y;
+    c2 = b * x + w * y;
+    costs.push(c1, c2);
+    return Math.min(...costs);
   }
   if (y > x && y > z) {
+    let costs = [];
     let blackToWhiteCost = x + z;
-    return b * x + w * y;
+    c1 = b * x + w * blackToWhiteCost;
+    c2 = b * x + w * y;
+    costs.push(c1, c2);
+    return Math.min(...costs);
   }
 }
-console.log(taumBday(10, 10, 1, 1, 1));
-console.log(taumBday(5, 9, 2, 3, 4));
-console.log(taumBday(3, 6, 9, 1, 1));
-console.log(taumBday(7, 7, 4, 2, 1));
-console.log(taumBday(3, 3, 1, 9, 2));
+
+function main() {
+  var t = parseInt(readLine());
+  for (var a0 = 0; a0 < t; a0++) {
+    var b_temp = readLine().split(' ');
+    var b = parseInt(b_temp[0]);
+    var w = parseInt(b_temp[1]);
+    var x_temp = readLine().split(' ');
+    var x = parseInt(x_temp[0]);
+    var y = parseInt(x_temp[1]);
+    var z = parseInt(x_temp[2]);
+    var result = taumBday(b, w, x, y, z);
+    process.stdout.write('' + result + '\n');
+  }
+}
