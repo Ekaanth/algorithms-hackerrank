cfd97195cf021837e519c30a719eaf735d7a54be
amalshehu <amalshehu@gmail.com>
2018-02-12 22:12:33 +0530

    Add test files

--- Implementation/flatland-space-stations/flatland-space-stations.js
+++ Implementation/flatland-space-stations/flatland-space-stations.js
@@ -1,3 +1,25 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = '';
+var input_stdin_array = '';
+var input_currentline = 0;
+
+process.stdin.on('data', function(data) {
+  input_stdin += data;
+});
+
+process.stdin.on('end', function() {
+  input_stdin_array = input_stdin.split('\n');
+  main();
+});
+
+function readLine() {
+  return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
 function flatlandSpaceStations(n, c) {
   cities = new Array(n).fill(0);
   distanceArray = [];
@@ -11,12 +33,20 @@ function flatlandSpaceStations(n, c) {
     prevIndex = j - 1;
     nextIndex = j + 1;
     while (cities[prevIndex] == 1 || cities[nextIndex] == 1) {
-      distanceArray.push(Math.min(prevIndex, nextIndex));
+      distanceArray.push(Math.min(j - prevIndex, j - nextIndex));
       prevIndex = prevIndex - 1;
       nextIndex = nextIndex + 1;
     }
   }
-  return Math.max(...distanceArray);
+  return Math.abs(Math.max(...distanceArray)) + 1;
 }
-flatlandSpaceStations(5, [0, 4]);
-flatlandSpaceStations(6, [0, 1, 2, 4, 3, 5]);
+
+function main() {
+  var n_temp = readLine().split(' ');
+  var n = parseInt(n_temp[0]);
+  var m = parseInt(n_temp[1]);
+  c = readLine().split(' ');
+  c = c.map(Number);
+  var result = flatlandSpaceStations(n, c);
+  process.stdout.write('' + result + '\n');
+}
