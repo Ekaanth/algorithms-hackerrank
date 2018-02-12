04bd299b6eb69516a5dfca42cb9e1ca45bf616b6
amalshehu <amalshehu@gmail.com>
2018-02-12 21:49:27 +0530

    Pass 3 tests

--- Implementation/flatland-space-stations/flatland-space-stations.js
+++ Implementation/flatland-space-stations/flatland-space-stations.js
@@ -1,15 +1,22 @@
 function flatlandSpaceStations(n, c) {
-  cities = new Array(n).fill(false);
+  cities = new Array(n).fill(0);
   distanceArray = [];
   c.forEach(i => {
-    cities[i] = true;
+    cities[i] = 1;
   });
-  count = 0;
-  for (let i = 0; i < n; i++) {
-    c.forEach(element => {
-      distanceArray.push(Math.abs(i - cities.indexOf(element)));
-    });
+  if (n === c.length) {
+    return 0;
   }
-  console.log(distanceArray);
+  for (let j = 0; j < cities.length; j++) {
+    prevIndex = j - 1;
+    nextIndex = j + 1;
+    while (cities[prevIndex] == 1 || cities[nextIndex] == 1) {
+      distanceArray.push(Math.min(prevIndex, nextIndex));
+      prevIndex = prevIndex - 1;
+      nextIndex = nextIndex + 1;
+    }
+  }
+  return Math.max(...distanceArray);
 }
 flatlandSpaceStations(5, [0, 4]);
+flatlandSpaceStations(6, [0, 1, 2, 4, 3, 5]);
