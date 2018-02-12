1508d4debb3f9249c23eb2865fe18812f272ced7
amalshehu <amalshehu@gmail.com>
2018-02-12 15:00:46 +0530

    Incomplete

--- Dynamic programming/stockmax/stockmax.js
+++ Dynamic programming/stockmax/stockmax.js
@@ -1,5 +1,5 @@
-function isIncreasingSequence(numbers) {
-  return numbers.every((x, i) => i === 0 || x >= arr[i - 1]);
+function isIncreasing(numbers) {
+  return numbers.every((x, i) => i === 0 || x >= numbers[i - 1]);
 }
 function profitCalculator(sharesBought, currentPrice) {
   let profit = 0;
@@ -11,33 +11,50 @@ function profitCalculator(sharesBought, currentPrice) {
   return profit;
 }
 function stockmax(prices) {
-  sharedBought = [];
   profit = 0;
-  currentPrice = 0;
-  for (let i = 0; i < prices.length; i++) {
-    currentPrice = prices[i];
-    if (prices[i] < prices[i + 1]) {
-      console.log(`${prices[i]} > ${prices[i + 1]}`);
-      sharedBought.push(prices[i]);
-    } else {
-      console.log('current', currentPrice);
-      let remainingPrices = prices.slice(i + 1);
-      console.log('remainingPrices', remainingPrices);
-
-      if (isIncreasingSequence(remainingPrices) && remainingPrices.length > 0) {
-        let lossArray = [];
-        for (let j = 0; j < remainingPrices.length; j++) {
-          lossArray.push(Math.abs(remainingPrices[j] - currentPrice));
-        }
-        console.log(lossArray);
-
-        profit += Math.min(...lossArray);
+  if (isIncreasing(prices)) {
+    sharesBought = [];
+    currentPrice = 0;
+    for (let i = 0; i < prices.length; i++) {
+      currentPrice = prices[i];
+      if (prices[i] < prices[i + 1]) {
+        sharesBought.push(prices[i]);
       }
     }
+    profit = profitCalculator(sharesBought, currentPrice);
+  } else if (!isIncreasing(prices) && Math.max(...prices) == prices[0]) {
+    return profit;
+  } else {
+    let sharesBought = [];
+    let currentPrice = 0;
+    for (let i = 0; i < prices.length; i++) {
+      currentPrice = prices[i];
+      if (currentPrice < prices[i + 1] && ) {
+        sharesBought.push(currentPrice);
+      }
+    }
+    console.log('Shares', sharesBought);
   }
   return profit;
 }
 
-// console.log(stockmax([1, 2, 100]));
-// console.log(stockmax([5, 3, 2]));
+console.log(stockmax([1, 2, 100]));
+console.log(stockmax([5, 3, 2]));
+
 console.log(stockmax([1, 3, 1, 2]));
+
+// else {
+//     console.log('current', currentPrice);
+//     let remainingPrices = prices.slice(i + 1);
+//     console.log('remainingPrices', remainingPrices);
+
+//   if (isIncreasing(remainingPrices) && remainingPrices.length > 0) {
+//     let lossArray = [];
+//     for (let j = 0; j < remainingPrices.length; j++) {
+//       lossArray.push(Math.abs(remainingPrices[j] - currentPrice));
+//     }
+//     console.log(lossArray);
+
+//     profit += Math.min(...lossArray);
+//   }
+//   }
