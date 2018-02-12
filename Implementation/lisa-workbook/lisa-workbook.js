e75771943a2a5b29889dc98d10a82e976928d528
amalshehu <amalshehu@gmail.com>
2018-02-12 17:19:26 +0530

    Add solution

--- Implementation/lisa-workbook/lisa-workbook.js
+++ Implementation/lisa-workbook/lisa-workbook.js
@@ -1,14 +1,28 @@
+function problemSplitter(array, size) {
+  var results = [];
+  while (array.length) {
+    results.push(array.splice(0, size));
+  }
+  return results;
+}
 function workbook(n, k, arr) {
   book = [];
+  count = 0;
   for (let i = 0; i < arr.length; i++) {
     page = [];
-    let pageCount = 0;
     currentProblem = arr[i];
     for (let j = 1; j <= arr[i]; j++) {
       page.push(j);
     }
-    book.push(page);
+    book.push(...problemSplitter(page, k));
   }
-  console.log(book);
+  let c = 1;
+  book.map(b => {
+    if (b.includes(c)) {
+      count++;
+    }
+    c++;
+  });
+  return count;
 }
 workbook(5, 3, [4, 2, 6, 1, 10]);
