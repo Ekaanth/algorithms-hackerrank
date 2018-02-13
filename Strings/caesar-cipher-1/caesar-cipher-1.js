3b8039582722b278e2e6998fa9ea9919d1e2b750
amalshehu <amalshehu@gmail.com>
2018-02-13 14:17:03 +0530

    Reformat code

--- Strings/caesar-cipher-1/caesar-cipher-1.js
+++ Strings/caesar-cipher-1/caesar-cipher-1.js
@@ -1,28 +1,18 @@
 function caesarCipher(s, k) {
-  //   console.log('Z'.charCodeAt(0));
-  //   console.log(String.fromCharCode(59));
   let encrypted = s.split('').map(char => {
     cc = char.charCodeAt(0);
     if (cc > 96 && cc < 123) {
-      if (cc + k % 26 <= 122) {
-        return String.fromCharCode(cc + k % 26);
-      } else {
-        let key = Math.abs(96 + cc - 122 + k % 26);
-        return String.fromCharCode(key);
-      }
+      return cc + k % 26 <= 122
+        ? String.fromCharCode(cc + k % 26)
+        : String.fromCharCode(Math.abs(96 + cc - 122 + k % 26));
     } else if (cc > 64 && cc < 91) {
-      if (cc + k % 26 <= 90) {
-        return String.fromCharCode(cc + k % 26);
-      } else {
-        let key = Math.abs(64 + cc - 90 + k % 26);
-        return String.fromCharCode(key);
-      }
+      return cc + k % 26 <= 90
+        ? String.fromCharCode(cc + k % 26)
+        : String.fromCharCode(Math.abs(64 + cc - 90 + k % 26));
     } else {
       return char;
     }
   });
-  console.log(encrypted);
-
   return encrypted.join('');
 }
 caesarCipher('Hello_World!', 4);
