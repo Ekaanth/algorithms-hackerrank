3acd9560bcb38b7874745a0f1287ec08f6fdda3b
amalshehu <amalshehu@gmail.com>
2018-02-15 15:27:54 +0530

    Add solution

--- Strings/game-of-thrones/game-of-thrones.js
+++ Strings/game-of-thrones/game-of-thrones.js
@@ -1,32 +1,15 @@
-function generatePermutations(array) {
-  let result = [];
-  array.sort();
-  for (let i = 0; i < array.length; i = i + 1) {
-    let rest = generatePermutations(
-      array.slice(0, i).concat(array.slice(i + 1))
-    );
-    if (!rest.length) {
-      result.push([array[i]]);
-    } else {
-      for (let j = 0; j < rest.length; j = j + 1) {
-        result.push([array[i]].concat(rest[j]));
-      }
-    }
-  }
-  return result;
-}
-function isPalindrome(str) {
-  let p = str.reverse().join('');
-  return p === str ? true : false;
-}
 function gameOfThrones(s) {
-  permutations = generatePermutations(s.split(''));
-  for (let i = 0; i < permutations.length; i++) {
-    current = permutations[i];
-    console.log(isPalindrome(current) ? 'YES' : 'NO');
-
-    return isPalindrome(current) ? true : false;
-  }
+  count = 0;
+  alphaCount = {};
+  s.split('').map(val => (alphaCount[val] = (alphaCount[val] || 0) + 1));
+  keys = Object.keys(alphaCount);
+  keys.map(key => {
+    if (alphaCount[key] % 2 !== 0) {
+      count++;
+    }
+  });
+  return count > 1 ? 'NO' : 'YES';
 }
-gameOfThrones('aaabbbb');
-gameOfThrones('cdefghmnopqrstuvw');
+console.log(gameOfThrones('aaabbbb'));
+
+console.log(gameOfThrones('cdefghmnopqrstuvw'));
