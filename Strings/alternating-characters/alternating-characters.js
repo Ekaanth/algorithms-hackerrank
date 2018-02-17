7d2ae271157147093da94c76455a3d200fc7a7ba
amalshehu <amalshehu@gmail.com>
2018-02-17 21:39:59 +0530

    Reformat solution

--- Strings/alternating-characters/alternating-characters.js
+++ Strings/alternating-characters/alternating-characters.js
@@ -1,29 +1,10 @@
-function eliminator(array) {
-  for (let i = 0; i < array.length; i++) {
-    current = array[i];
-    next = array[i + 1];
-    if (current === next && array.length >= 2) {
-      array.splice(i + 1, 1);
-    }
-  }
-  return array;
-}
-function checker(array) {
-  for (let i = 0; i < array.length; i++) {
-    current = array[i];
-    next = array[i + 1];
-    if (array[i] === next && array.length >= 2) {
-      return true;
-    } else false;
-  }
-}
 function alternatingCharacters(s) {
-  const length = s.split('').length;
   stringArray = s.split('');
-  do {
-    stringArray = eliminator(stringArray);
-  } while (checker(stringArray));
-  return length - stringArray.length;
+  count = 0;
+  for (let i = 0; i < stringArray.length; i++) {
+    stringArray[i] === stringArray[i + 1] ? count++ : null;
+  }
+  return count;
 }
 alternatingCharacters('AAAA');
 alternatingCharacters('BBBBB');
