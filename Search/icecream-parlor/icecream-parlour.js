6a194d087b2bf6cd5faf60524fe4c96bb4901f50
amalshehu <amalshehu@gmail.com>
2018-02-17 23:09:34 +0530

    Fix typos

--- Search/icecream-parlor/icecream-parlour.js
+++ Search/icecream-parlor/icecream-parlour.js
@@ -2,8 +2,7 @@ function icecreamParlor(m, arr) {
   iceCreamId = {};
   for (let i = 0; i < arr.length; i++) {
     iceCreamId[arr[i]] > 0 ? console.log(iceCreamId[arr[i]], i + 1) : null;
-
     arr[i] < m ? (iceCreamId[m - arr[i]] = i + 1) : null;
   }
 }
-console.log(icecreamParlor(4, [1, 4, 5, 3, 2]));
+icecreamParlor(4, [1, 4, 5, 3, 2]);
