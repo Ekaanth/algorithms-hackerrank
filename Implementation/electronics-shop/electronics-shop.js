93760380aac0dba77e26e6fc92e5a2acf7f713e1
amalshehu <amalshehu@gmail.com>
2018-02-20 02:44:43 +0530

    Upgrade solution

--- Implementation/electronics-shop/electronics-shop.js
+++ Implementation/electronics-shop/electronics-shop.js
@@ -1,14 +1,16 @@
-function getMoneySpent(keyboards, drives, s){
-  costArray = []
+function getMoneySpent(keyboards, drives, s) {
+  highestCost = 0;
   for (let i = 0; i < keyboards.length; i++) {
     for (let j = 0; j < drives.length; j++) {
-      let cost = keyboards[i] + drives[j]
-      if (cost < s){
-        costArray.push(cost)
+      let cost = keyboards[i] + drives[j];
+      if (cost <= s) {
+        if (cost > highestCost) {
+          highestCost = cost;
+        }
       }
     }
   }
-  if(costArray.length > 0){
-    return Math.max(...costArray)
-  }else return -1
+  if (highestCost > 0) {
+    return highestCost;
+  } else return -1;
 }
