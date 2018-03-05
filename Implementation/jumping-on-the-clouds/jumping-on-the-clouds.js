3ee67a466fcd927a438c7926db3d572b00ef49f9
amalshehu <amalshehu@gmail.com>
2018-03-05 14:48:08 +0530

    Add solution

--- Implementation/jumping-on-the-clouds/jumping-on-the-clouds.js
+++ Implementation/jumping-on-the-clouds/jumping-on-the-clouds.js
@@ -1,15 +1,15 @@
 function jumpingOnClouds(c) {
   jumpCount = 0;
   for (let i = 0; i <= c.length; i++) {
-    if (c[i] == 0 && c[i + 1] == 0) {
-      i += 2;
-      jumpCount += 2;
-    } else {
+    if (c[i + 1] == 0 && c[i + 2] == 0) {
+      i += 1;
+      jumpCount++;
+    } else if (c[i + 1] == 1 && c[i + 2] == 0) {
       i++;
       jumpCount++;
-    }
+    } else jumpCount++;
   }
-  return jumpCount - 1;
+  return jumpCount - 2;
 }
 jumpingOnClouds([0, 0, 1, 0, 0, 1, 0]);
 jumpingOnClouds([0, 0, 0, 0, 1, 0]);
