906b6449345f5aa176533873405cf7d76c92a7e3
amalshehu <amalshehu@gmail.com>
2018-03-05 22:49:09 +0530

    Add solution

--- Strings/weighted-uniform-string/weighted-uniform-string.js
+++ Strings/weighted-uniform-string/weighted-uniform-string.js
@@ -1,30 +1,12 @@
-function generateSubArrays(arr) {
-  subs = [];
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i; j < arr.length; j++) {
-      let ar = [];
-      for (let k = i; k <= j; k++) {
-        ar.push(arr[k]);
-      }
-      const isUniform = x => x === ar[0];
-      if (ar.every(isUniform)) {
-        subs.push(ar);
-      }
-    }
-  }
-  return subs;
-}
 function getStringWeight(s, x) {
   alpha = '0abcdefghijklmnopqrstuvwxyz'.split('');
-  sub = generateSubArrays(s.split(''));
-  const subValue = sub.map(arr => {
-    if (arr.length === 1) {
-      return alpha.indexOf(arr[0]);
-    } else {
-      return alpha.indexOf(arr[0]) * arr.length;
-    }
-  });
-
-  return subValue.includes(x) ? 'Yes' : 'No';
+  counter = 1;
+  const subValue = new Set([]);
+  for (let i = 0; i < s.length; i++) {
+    subValue.add(alpha.indexOf(s[i]));
+    i + 1 != s.length && s[i + 1] == s[i] ? (counter += 1) : (counter = 1);
+    subValue.add(alpha.indexOf(s[i]) * counter);
+  }
+  return subValue.has(x) ? 'Yes' : 'No';
 }
 console.log(getStringWeight('abccddde', 6));
